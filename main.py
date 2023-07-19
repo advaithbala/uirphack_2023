d3876c23cf127ded8e4f678319b828bd43a39104
advaith <advaithbala@gmail.com>
2023-07-19 17:42:28 -0500

    fixed stuff

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pygame
 import sys
 import random
 import platform
-from pygame.locals import *
+# import subprocess
 from repair import repair
 WINDOW_WIDTH = 1024
 WINDOW_HEIGHT = 768
@@ -108,7 +108,6 @@ def draw_car(draw_surface: pygame.Surface, isTent):
         draw_surface.blit(scaled_sprite, (carX, carY))
 
 
-
 def draw_distance(draw_surface: pygame.Surface, distance):
 
     font = pygame.font.Font('PixeloidSansBold-PKnYd.ttf', 32)
@@ -203,13 +202,9 @@ class GameWindow:
         self.crashed_CD = 0
         self.battery_max = 7777
         self.battery_level = self.battery_max
-
         self.game_started = False
-
-
         self.tesla = pygame.transform.scale(pygame.image.load("./static/tesla.png"), (150, 100))
         self.flame = pygame.transform.scale(pygame.image.load("./static/flame.png"), (150, 100))
-
 
         # background
         if platform.system() == "Darwin":
@@ -243,21 +238,16 @@ class GameWindow:
                 self.sprites.append(pygame.image.load(f"images/{i}.png").convert())
             else: 
                 self.sprites.append(pygame.image.load(f"images/{i}.png").convert_alpha())
-
-        #make sprite[8] of size 100x100
-        self.sprites[7] = pygame.transform.scale(self.sprites[7], (60, 90))
-    prevPlayerX = 0
-    
     def draw_enter_page(self):
         # Draw the transparent mask over the game window
         # mask = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.SRCALPHA)
         #set a transparent color 
         mask = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.SRCALPHA)
-        mask.fill((0, 0, 0, 2))  # Semi-transparent black
+        mask.fill((0, 0, 0, 0))  # Semi-transparent black
         self.window_surface.blit(mask, (0, 0))
 
         # Draw the "Start Playing" button with rounded corners
-        button_width = 200
+        button_width = 250
         button_height = 80
         button_rect = pygame.Rect(
             (WINDOW_WIDTH - button_width) // 2,
@@ -280,13 +270,13 @@ class GameWindow:
         pygame.draw.rect(self.window_surface, button_color, button_rect, border_radius=10)
 
         # Draw the "Start Playing" button text
-        font = pygame.font.Font('freesansbold.ttf', 28)  # Smaller font size
+        font = pygame.font.Font('PixeloidSansBold-PKnYd.ttf', 28)  # Smaller font size
         text = font.render("Start Playing", True, (0, 0, 0))
         self.text_rect = text.get_rect(center=button_rect.center)
         self.window_surface.blit(text, self.text_rect)
 
         # Set up the font for the game title
-        title_font = pygame.font.SysFont("FixedSys", 48)
+        title_font = pygame.font.Font("PixeloidSansBold-PKnYd.ttf", 48)
 
         # Render the game title text
         title_text = title_font.render("Rivian Adventure", True, (255, 255, 255))
@@ -297,8 +287,10 @@ class GameWindow:
         # Draw the game title on the window surface
         self.window_surface.blit(title_text, title_rect)
 
-   
+    prevPlayerX = 0
+
     def run(self):
+
         # create road lines for each segment
         lines: List[Line] = []
         for i in range(1600):
@@ -378,7 +370,6 @@ class GameWindow:
         start_time = time.time()
 
         while True:
-
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -391,20 +382,34 @@ class GameWindow:
             if not self.game_started:
                 self.draw_enter_page()
             else:
-                
+
+                out_of_bounds = (playerX > 3000) or (playerX < -3000)
+
                 self.dt = time.time() - self.last_time
                 self.last_time = time.time()
                 self.window_surface.fill((105, 205, 4))
+
+                for event in pygame.event.get([pygame.QUIT]):
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        sys.exit()
 
                 speed = 0
                 keys = pygame.key.get_pressed()
                 if keys[pygame.K_t]:
                     isTent = not isTent
+
                 if keys[pygame.K_UP]:
                     if self.battery_level == 0:
                         continue
-                    speed += segL  # it has to be N integer times the segment length
-                    self.battery_level = max(0, self.battery_level - 10)
+                    if(out_of_bounds):
+                        speed += segL # it has to be N integer times the segment length
+                        self.battery_level = max(0, self.battery_level - 2)
+                    else:   
+                        speed += (segL * 2) # it has to be N integer times the segment length
+                        self.battery_level = max(0, self.battery_level - 10)
+
+
                 if keys[pygame.K_DOWN]:
                     if self.battery_level == 0:
                         continue              
@@ -412,11 +417,11 @@ class GameWindow:
                 if keys[pygame.K_RIGHT]:
                     if self.battery_level == 0:
                         continue
-                    playerX += 100
+                    playerX += 50
                 if keys[pygame.K_LEFT]:
                     if self.battery_level == 0:
                         continue
-                    playerX -= 100
+                    playerX -= 50
                 if keys[pygame.K_w]:
                     playerY += 100
                 if keys[pygame.K_s]:
@@ -428,6 +433,7 @@ class GameWindow:
                 if keys[pygame.K_TAB]:
                     speed *= 2  # it has to be N integer times the segment length
                     self.battery_level = max(0, self.battery_level - 30)
+
                 pos += speed
 
                 # loop the circut from start to finish
@@ -506,11 +512,27 @@ class GameWindow:
                 for n in range(startPos + show_N_seg, startPos + 1, -1):
                     lines[n % N].drawSprite(self.window_surface)
 
-                out_of_bounds = (playerX > 3000) or (playerX < -3000)
-                deployTent = out_of_bounds and isTent
-
-                draw_car(self.window_surface, deployTent)
+                draw_car(self.window_surface, out_of_bounds)
                 draw_battery(self.window_surface, self.battery_level, self.battery_max)
+
+                draw_distance(self.window_surface, pos)
+
+                # Calculate time left
+                elapsed_time = time.time() - start_time
+                time_left = max(timer_duration - int(elapsed_time), 0)
+
+                draw_timer(self.window_surface, time_left)
+                pygame.display.update()
+
+                #print car position
+                # print(f"Car position: {playerX}, {playerY}")
+                if self.battery_level == 0:
+                    # print(f"Car position: {playerX}, {playerY}")
+                    self.ret = repair()
+                    # self.ret = subprocess.Popen(["python", "./repair.py"])
+                    # print(f"|-> Repairing cost {self.ret} seconds")
+                    if self.ret is not None:
+                        self.battery_level = self.battery_max  # Or some code to reset the game state
 
                 # Pick up Battery
                 line_has_battery = lines[startPos % N].battery_pos != None
@@ -518,14 +540,14 @@ class GameWindow:
                     battery_pos = lines[startPos % N].battery_pos
                     battery_abs_pos = map_value(battery_pos, -3, 2, -1900, 1900)
                     if abs (battery_abs_pos - playerX) < 400:
-                        self.battery_level = min(self.battery_max, self.battery_level + 1500)
+                        self.battery_level = min(self.battery_max, self.battery_level + 500)
 
                 # Collide with tesla
                 line_has_tesla = lines[startPos % N].tesla_pos != None
                 if not prev_line_has_tesla and line_has_tesla:
                     tesla_pos = lines[startPos % N].tesla_pos
                     tesla_abs_pos = map_value(tesla_pos, -3, 2, -1900, 1900)
-                    if abs (tesla_abs_pos - (playerX)) < 850:
+                    if abs (tesla_abs_pos - (playerX)) < 500:
                         #show the flame here
                         self.crashed_CD = 5 
                         self.window_surface.blit(self.flame, (WINDOW_WIDTH//2 - 50, WINDOW_HEIGHT//2 - 50))
@@ -542,7 +564,6 @@ class GameWindow:
                 prev_line_has_battery = line_has_battery
             pygame.display.update()
             self.clock.tick(60)
-
 
             
 if __name__ == "__main__":
